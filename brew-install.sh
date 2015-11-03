aca10baa7d1842f3921bc45376ea522bf1e57616
Naveen Kasthuri <naveen.kasthuri@addepar.com>
2015-11-03 11:27:47 -0500

    Create brew-install.sh

---
+++ brew-install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Homebrew.
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+  then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+  fi
+  
+fi
+
+# Install homebrew packages
+brew update
+brew install brew-cask autojump git mysql q vim zsh
+brew-cask install caffeine flux google-chrome iterm2 sequel-pro
+exit 0
